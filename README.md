6b181ba2cad3800be48534b4ff7aeb4d2611f1ce
2333Ge <ych.class@qq.com>
2023-03-29 11:08:50 +0800

    feat:css-rnstyle互相转换 0.0.2

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# demo
+# 将css样式和RN样式互相转换
 
-f5运行
+快捷键:
+- `ctrl + 9`：css转RN style
+- `ctrl + 8`：RN style转css
+
+# 示例：
+
+片段1：
+
+```css
+position: static;
+left: 0px;
+top: 0px;
+width: 48px;
+height: 48px;
+opacity: 1;
+
+z-index: 0;
+```
+
+片段2：
+
+```js
+{
+  position: 'static',
+  left: 0,
+  top: 0,
+  width: 48,
+  height: 48,
+  opacity: 1,
+  zIndex: 0,
+}
+```
+
+`ctrl + 9` 将片段1转换为片段2
+
+`ctrl + 8` 将片段2转换为片段1
--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,92 @@
-
-import * as vscode from 'vscode';
-import { loadConfig, updateConfig } from './config';
-
-import { highlightTargetStr } from './highlight';
-import { showQuickPick } from './quickPick';
-import { displayStatusBar } from './StatusBar';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	highlightTargetStr('primary');
-	loadConfig();
-	displayStatusBar();
-	context.subscriptions.push(
-		// 测试代码更新config
-		vscode.commands.registerCommand('highlight-my-word-api-demo.helloWorld', () => {
-			vscode.window.showInformationMessage('Hello World from highlight-my-word-api-demo!');
-			updateConfig();
-		}),
-		vscode.commands.registerCommand('highlight-my-word-api-demo.changeTheme', showQuickPick),
-		vscode.workspace.onDidChangeConfiguration((e) => {
-			console.log(e);
-		}),
-		vscode.window.onDidChangeVisibleTextEditors(editors => { console.log(editors.length); }),
-		vscode.languages.registerHoverProvider(['javascript', 'typescript'], {
-			provideHover: (document: vscode.TextDocument, position: vscode.Position, _: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> => {
-				// 得到鼠标悬浮处的单词
-				const word = document.getText(document.getWordRangeAtPosition(position));
-				if (word === 'primary') {
-					return new vscode.Hover('悬浮窗');
-				}
-			},
-		})
-	);
+  let disposableToRnStyle = vscode.commands.registerCommand(
+    "css-to-rnstyle.convertCssToRnStyle",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+
+      const document = editor.document;
+      const selection = editor.selection;
+      const text = document.getText(selection);
+
+      const convertedText = convertCssToRnStyle(text);
+
+      editor.edit((editBuilder) => {
+        editBuilder.replace(selection, convertedText);
+      });
+    }
+  );
+  let disposableToCss = vscode.commands.registerCommand(
+    "css-to-rnstyle.convertRnStyleToCss",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+
+      const document = editor.document;
+      const selection = editor.selection;
+      const text = document.getText(selection);
+
+      const convertedText = convertRnStyleToCss(text);
+
+      editor.edit((editBuilder) => {
+        editBuilder.replace(selection, convertedText);
+      });
+    }
+  );
+  context.subscriptions.push(disposableToRnStyle, disposableToCss);
 }
 
-export function deactivate() { }
+function convertCssToRnStyle(cssText: string): string {
+  let result = cssText;
+  result = result.replace(/(\d+)px/g, "$1");
+  result = result.replace(/(\d+)em/g, "$1");
+  result = result.replace(/-(\w)/g, (_, c) => c.toUpperCase());
+  result = result.replace(/;/g, ",");
+  result = result.replace(/:\s*(.*),/g, (match, str: string) => {
+    if (isNaN(Number(str.trim()))) {
+      return match.replace(str, `"${str}"`);
+    }
+    return match;
+  });
+  return result;
+}
+
+/**
+ * 默认会将number类型的属性加上px除了这些
+ */
+const NOT_TRANSFORM_RN_NUM_KEYS = ["opacity", "zIndex", "elevation"];
+
+function convertRnStyleToCss(cssText: string): string {
+  let result = cssText;
+  result = result.replace(/,/g, ";");
+  result = result.replace(
+    /(\w+)\s*:\s*(.+);/g,
+    (match, p1: string, p2: string) => {
+      if (p2.indexOf('"') !== -1) {
+        return match.replace(new RegExp('"', "g"), "");
+      }
+      if (p2.indexOf("'") !== -1) {
+        return match.replace(new RegExp("'", "g"), "");
+      }
+      if (
+        !isNaN(Number(p2.trim())) &&
+        NOT_TRANSFORM_RN_NUM_KEYS.indexOf(p1.trim()) === -1
+      ) {
+        return match.replace(p2, `${p2}px`);
+      }
+      return match;
+    }
+  );
+  result = result.replace(/[a-z]([A-Z])\w+:/g, (match, c) => {
+    return match.replace(c, `-${c.toLowerCase()}`);
+  });
+  return result;
+}
+
+export function deactivate() {}
